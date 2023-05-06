4923d1e0a03d9406af0d9464bb751d50dc14cb58
Piotr Ćwikliński <piotr.cwiklinski88@gmail.com>
2023-05-06 09:30:06 +0200

    dodana akcja btnClicked

--- src/MyComponents/Main.js
+++ src/MyComponents/Main.js
@@ -1,8 +1,20 @@
 function Main() {
+
+  const btnClick=()=>{
+    alert("btn clicked");
+  } 
+
   return (
     <div>
-      <h2>Main works</h2>
-      <button type="button" class="btn btn-danger">Button</button>
+
+      <div className="card border-primary m-5 shadow-lg">
+        <div className="card-body">
+          <h4 className="card-title">My First Code</h4>
+          <button onClick={btnClick} type="button" className="btn btn-danger">
+            Button
+            </button>
+        </div>
+      </div>
     </div>
   );
 }
